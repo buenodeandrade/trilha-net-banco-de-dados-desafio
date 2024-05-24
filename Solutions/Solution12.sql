b2397bc63a1208bfef09cc1bd989f307bc8bcbb2
Felipe Bueno de Andrade <147355115+buenodeandrade@users.noreply.github.com>
2024-05-24 12:28:50 -0300

    Add files via upload

---
+++ Solutions/Solution12.sql
@@ -0,0 +1,11 @@
+SELECT 
+    f.Nome AS Nome,
+    a.PrimeiroNome,
+    a.UltimoNome,
+    ef.Papel
+FROM 
+    Filmes f
+JOIN 
+    ElencoFilme ef ON f.Id = ef.IdFilme
+JOIN 
+    Atores a ON ef.IdAtor = a.Id;
